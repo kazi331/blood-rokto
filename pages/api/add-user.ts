5b13bd7ee950c10f7881ae28c36e748e7f1912d1
Kazi Shariful Islam <96835101+kazi331@users.noreply.github.com>
2022-11-08 17:23:06 +0600

    api directory disabled

--- pages/api/add-user.ts
+++ pages/api/add-user.ts
@@ -1,16 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import clientPromise from "../../lib/mongodb";
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import clientPromise from "../../lib/mongodb";
 
-const handler = async(req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body)
-  const data = JSON.parse(req.body)
-  try {
-    const client = await clientPromise;
-    const db = client.db('users')
-    const user = await db.collection('users').insertOne(data)
-    res.status(200).send(user)
-  } catch (error) {
-    console.log(error)
-  }
-}
-export default handler;
+// const handler = async(req: NextApiRequest, res: NextApiResponse) => {
+//   console.log(req.body)
+//   const data = JSON.parse(req.body)
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db('users')
+//     const user = await db.collection('users').insertOne(data)
+//     res.status(200).send(user)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// export default handler;
--- pages/api/addPost.ts
+++ pages/api/addPost.ts
@@ -1,22 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import clientPromise from "../../lib/mongodb";
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import clientPromise from "../../lib/mongodb";
 
-const handler = async(req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("posts");
-    const { title, content } = req.body;
+// const handler = async(req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("posts");
+//     const { title, content } = req.body;
 
-    const post = await db.collection("posts").insertOne({
-      title,
-      content,
-    });
+//     const post = await db.collection("posts").insertOne({
+//       title,
+//       content,
+//     });
 
-    res.json(post);
-  } catch (err) {
-    console.log(err);
+//     res.json(post);
+//   } catch (err) {
+//     console.log(err);
 
-  }
-};
+//   }
+// };
 
-export default handler;
+// export default handler;
