f893042b79dbbb4c6eeef36527f237ba29dfe4da
kazi331 <kazisharifulislam52@gmail.com>
2022-11-10 23:22:04 +0600

    none

--- page-components/NavBar.tsx
+++ page-components/NavBar.tsx
@@ -9,6 +9,7 @@ const pages = [
   { slug: '/campaign', label: 'Campaign' },
   { slug: '/login', label: 'Login' },
   { slug: '/register', label: 'Registration' },
+  { slug: '/dashboard', label: 'Dashboard' },
 ]
 
 
@@ -25,7 +26,7 @@ const NavBar = () => {
         </div>
 
         {/* mobile menu  */}
-        <div onClick={(e) => setToggleMenu(false)} className={`${toggleMenu ? 'bottom-0' : '-bottom-full overflow-hidden'} flex top-100 h-screen w-full p-4 py-8 transition-all duration-300 justify-center items-end md:hidden fixed left-0  bg-gray-900 bg-opacity-80 backdrop-blur-sm text-gray-200 z-10 `}>
+        <div onClick={(e) => setToggleMenu(false)} className={`${toggleMenu ? 'bottom-0' : '-bottom-full p-0 m-0 overflow-hidden'} flex top-100 h-screen w-full p-4 py-8 transition-all duration-300 justify-center items-end md:hidden fixed left-0  bg-gray-900 bg-opacity-80 backdrop-blur-lg text-gray-200 z-10 `}>
           <div className="flex flex-col w-full md:max-w-xs bg-gray-800 bg-opacity-40 p-3 rounded backdrop-blur">
             {pages.map((page, i) => <Link key={i} href={page.slug} className='hover:bg-gray-600 hover:bg-opacity-40 rounded p-1 px-2 transition min-w-max '>{page.label}</Link>)}
           </div>
@@ -33,7 +34,7 @@ const NavBar = () => {
 
         {/* toggle */}
         <div className="fixed bottom-10 right-6 z-20">
-          <button className='block md:hidden  text-white bg-gray-800 bg-opacity-60 backdrop-blur-lg p-2 text-2xl rounded-3xl shadow-sm shadow-gray-900' onClick={() => setToggleMenu(!toggleMenu)}>
+          <button className='block md:hidden outline-none active:outline-none text-white bg-gray-800 bg-opacity-60 backdrop-blur-lg p-2 text-2xl rounded-3xl shadow-sm shadow-gray-900' onClick={() => setToggleMenu(!toggleMenu)}>
             {toggleMenu ? <XMarkIcon className='w-6 h-6' /> : <Bars3Icon className='w-6 h-6' />}
           </button>
         </div>
