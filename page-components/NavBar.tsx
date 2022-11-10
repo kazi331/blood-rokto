9f0b62b4152cbcd685fc13eba6f550ca27f75956
kazi331 <kazisharifulislam52@gmail.com>
2022-11-10 13:43:08 +0600

    mobile menu temp done

--- page-components/NavBar.tsx
+++ page-components/NavBar.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import { Bars3Icon, BeakerIcon, LockClosedIcon, XCircleIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link';
 import { useState } from 'react';
 import { CloseRound, ToggleRound } from './Icons';
 
@@ -19,18 +20,21 @@ const NavBar = () => {
         </div>
 
         {/* mobile menu  */}
-        <div className={`${toggleMenu ? 'h-screen p-4 py-8' : 'h-0 overflow-hidden p-0'} flex transition-all justify-center items-end md:hidden fixed left-0 bottom-0 w-full h-screen bg-gray-900 bg-opacity-40 backdrop-blur-lg text-gray-200 z-10 `}>
-          <div className="flex flex-col w-full max-w-sm">
-            <Link href="/" className='hover:bg-gray-700 hover:bg-opacity-20 rounded p-1 px-3 transition min-w-max '>Home</Link>
-            <Link href="/about" className='hover:bg-gray-700 hover:bg-opacity-20 rounded p-1 px-3 transition min-w-max '>About</Link>
-            <Link href="/campaign" className='hover:bg-gray-700 hover:bg-opacity-20 rounded p-1 px-3 transition min-w-max '>Campaign</Link>
-            <Link href="/contact" className='hover:bg-gray-700 hover:bg-opacity-20 rounded p-1 px-3 transition min-w-max '>Contact</Link>
-            <Link href="/login" className='hover:bg-gray-700 hover:bg-opacity-20 rounded p-1 px-3 transition min-w-max '>Login</Link>
+        <div className={`${toggleMenu ? 'h-screen p-4 py-8' : 'h-0 overflow-hidden p-0'} flex transition-all justify-center items-end md:hidden fixed left-0 bottom-0 w-full h-screen bg-gray-900 bg-opacity-80 backdrop-blur-sm text-gray-200 z-10 `}>
+          <div className="flex flex-col w-full max-w-xs">
+            <Link href="/" className='active:bg-gray-600 active:bg-opacity-40 rounded p-1 px-3 transition min-w-max '>Home</Link>
+            <Link href="/about" className='active:bg-gray-600 active:bg-opacity-40 rounded p-1 px-3 transition min-w-max '>About</Link>
+            <Link href="/campaign" className='active:bg-gray-600 active:bg-opacity-40 rounded p-1 px-3 transition min-w-max '>Campaign</Link>
+            <Link href="/contact" className='active:bg-gray-600 active:bg-opacity-40 rounded p-1 px-3 transition min-w-max '>Contact</Link>
+            <Link href="/login" className='active:bg-gray-600 active:bg-opacity-40 rounded p-1 px-3 transition min-w-max '>Login</Link>
           </div>
         </div>
 
+        {/* toggle */}
         <div className="fixed bottom-10 right-10 z-20">
-          <button className='block md:hidden  text-white bg-gray-800 bg-opacity-40 backdrop-blur-lg p-1 text-2xl rounded shadow-sm shadow-gray-900' onClick={() => setToggleMenu(!toggleMenu)}> {toggleMenu? <CloseRound/> : <ToggleRound /> }</button>
+          <button className='block md:hidden  text-white bg-gray-800 bg-opacity-60 backdrop-blur-lg p-2 text-2xl rounded-3xl shadow-sm shadow-gray-900' onClick={() => setToggleMenu(!toggleMenu)}>
+            {toggleMenu ? <XMarkIcon className='w-6 h-6' /> : <Bars3Icon className='w-6 h-6' />}
+          </button>
         </div>
       </div>
     </nav>
